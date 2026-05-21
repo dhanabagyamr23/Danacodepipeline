02f3d032e9e00fda41ca542b2839e28858f10d8b
Ubuntu <ubuntu@ip-172-31-45-248.ap-south-1.compute.internal>
2026-05-21 23:14:18 +0000

    final deployment test

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-  res.write('CodePipeline Deployment Success');
-  res.end();
-}).listen(3000);
-
-console.log("Running on port 3000");
+GitHub Actions Auto Deployment Working
